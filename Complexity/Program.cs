e25b945a042a5ef61039f5d70d6388e46053e84e
Mikhail Koval <kovalm1995@gmail.com>
2020-02-23 09:15:00 +0200

    Add project files.

---
+++ Complexity/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Complexity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int convertNumber = int.Parse(number);
+            int i = convertNumber - 1;
+
+            while (convertNumber % i != 0)
+            {
+                i--;
+            }
+            Console.WriteLine(i);
+        }
+    }
+}
